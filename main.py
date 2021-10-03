1d32503743c8d89f3b2213a3836adfc703786940
AlmightyGxd <91399920+AlmightyGxd@users.noreply.github.com>
2021-10-03 11:43:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 choice = input("Rifle or Shotgun? ")
 if choice == "Rifle":
-    choice2 = input("Input Type (Ex. AR, M4)")
-    print(f'/me Unslings{choice2} turning safety [OFF]')
+    choice2 = input("Input Type (Ex. AR, M4) ")
+    print(f'/me Unslings {choice2} turning safety [OFF]')
 elif choice == "Shotgun":
     choice3 = input("Input Shotgun")
-    print(f'/me Unslings{choice3} pumping one into the chamber')
+    print(f'/me Unslings {choice3} pumping one into the chamber')
 else:
     print("Invalid input")
